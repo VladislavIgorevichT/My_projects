ab185db554d3d66e26b16dde45906987883444f0
VladislavT <byavm73@yandex.ru>
2026-06-29 13:47:24 +0400

    Решаю задачки

---
+++ src/digit_stats.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 0;
+    int sum = 0;
+    int even = 0;
+    int max_number = 0;
+    int reversed = 0;
+
+    scanf("%d", &number);
+
+    if (number < 0) {
+        number = -number;
+    }
+
+    if (number == 0) {
+        even = even + 1;
+    }
+
+    while (number != 0) {
+        // sum
+        int digit = number % 10;
+        sum = sum + digit;
+        // even digit
+        if (digit % 2 == 0) {
+            even = even + 1;
+        }
+        // max
+        if (max_number < digit) {
+            max_number = digit;
+        }
+        //reversed
+        reversed = reversed * 10 + digit;
+
+        number = number / 10;
+    }
+
+    printf("sum: %d\n", sum);
+    printf("even digits: %d\n", even);
+    printf("max digit: %d\n", max_number);
+    printf("reversed: %d\n", reversed);
+    return 0;
+}
---
+++ src/mod10_task.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number = 0;
+    int max_digit = 0;
+    if (scanf("%d", &number) == 1) {
+        while (number != 0) {
+            if (number % 10 > max_digit) {
+                max_digit = number % 10;
+            } 
+            number = number / 10;
+        }
+        printf("%d", max_digit);
+    }
+    return 0;
+}
