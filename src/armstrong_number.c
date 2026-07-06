054f0d6398db5d5662bf7d5f8a209ac7ac27d1db
VladislavT <byavm73@yandex.ru>
2026-07-06 15:13:05 +0400

    armstrong_number

---
+++ src/armstrong_number.c
@@ -0,0 +1,35 @@
+//Число армстронга (да/нет), принимает на вход положительное целое число, 
+//если сумма его цифр возведенных в степень кол-ва числе равна самому числу.
+
+#include <stdio.h>
+
+int main(void) {
+    int number = 0;
+    scanf("%d", &number);
+
+    int degree = number;
+    int sum = 0;
+    int power = 1;
+    int step = 0;
+    int digit = number;
+
+    while(degree != 0) {
+        degree /= 10;
+        step++;
+    }
+
+    while(digit != 0) {
+        for (int i = 0; i < step; i++) {
+            power *= digit % 10;
+        }
+        sum += power;
+        digit /= 10;
+        power = 1;
+    }
+
+    if (sum == number) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+}
